716025565ee41f79e90938be2ea7bd87eec34c8f
euihyeon <euihyeon.k@gmali.com>
2021-07-23 02:28:37 +0900

    git push origin master

---
+++ app.js
@@ -0,0 +1,7 @@
+function send(){
+   const msg = document.getElementById('message').value;
+   const table = document.getElementById('table');
+   const newRow = table.insertRow();
+   const newCell = newRow.insertCell(0);
+   newCell.innerText =msg;
+}
